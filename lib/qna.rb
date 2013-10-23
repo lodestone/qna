3b9ed10cb365c317da5e7c7e5db8594b51dc776b
Matt Petty <matt@kizmeta.com>
2013-10-23 15:26:53 -0500

    Remove some @s

--- lib/qna.rb
+++ lib/qna.rb
@@ -5,17 +5,17 @@ class QnA
   attr_reader :questions, :answers
 
   def initialize(input, size=4, answers_outfile='answers.txt', questions_outfile='questions.txt')
-    answers, questions = parse(standardize_input(input), size.to_i)
-    write_questions_and_answers(answers, questions, answers_outfile, questions_outfile)
+    @answers, @questions = parse(standardize_input(input), size.to_i)
+    write_questions_and_answers(@answers, @questions, answers_outfile, questions_outfile)
   end
 
   def standardize_input(input)
-    @words_string = File.exists?(input) ? File.open(input).read : input
+    File.exists?(input) ? File.open(input).read : input
   end 
 
   def parse(word_string, size)
-    @answers = []
-    @questions = []
+    answers = []
+    questions = []
     word_scanner = StringScanner.new(word_string)
     word_string.split("\n").each do |word|
       (0..word.length-1).each do |i| 
@@ -25,12 +25,12 @@ class QnA
         regexp = Regexp.compile(match)
         word_scanner.scan_until(regexp)
         if !word_scanner.scan_until(regexp)
-          @answers << word
-          @questions << match
+          answers << word
+          questions << match
         end
       end
     end
-    [@answers, @questions]
+    [answers, questions]
   end 
 
   def write_questions_and_answers(answers, questions, answers_outfile, questions_outfile)
